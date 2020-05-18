3a76794532fa0d679fa6b01282093ea25b5a0c38
Sandie Keerstock <64974352+skrstck@users.noreply.github.com>
2020-05-18 11:53:36 -0500

    tweak "about"

--- js/main.js
+++ js/main.js
@@ -20,6 +20,10 @@ $(document).ready(function () {
   if (window.location.hash && window.location.hash == '#blog') {
     $('.panel-cover').addClass('panel-cover--collapsed')
   }
+    
+  if (location.pathname == 'about') {
+    $('.panel-cover').addClass('panel-cover--collapsed')
+  }
 
   if (window.location.pathname !== '{{ site.baseurl }}/' && window.location.pathname !== '{{ site.baseurl }}/index.html') {
     $('.panel-cover').addClass('panel-cover--collapsed')
